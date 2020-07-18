f8c20932e772e9ba6aed240265b89e02a9be38d2
Igor Kolodiazhnyi <itsfunnytoseeyouhere@gmail.com>
2020-07-18 12:23:31 +0300

    Add 'Post' model, templates, views, urls.
    'Post' model - fields: title, slug, body, date_pub; methods: get_absolute_url, __str__.
    'index.html' template - displays post lists with date_pub, title and truncated body parameters. Has opportunity read the whole post by clicking 'Read' button.
    'post_detail.html' template - displays post with full body parameter.
    'post/<str:slug>/' url - new url for redirection to certain post.
    'post_detail' view - get a post with certain slug parameter.

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Post
+
 
 def posts_list(request):
-    names = ['Igor Kolodiazhnyi', 'I', 'Me']
-    return render(request, 'blog/index.html', context={'author': names})
+    posts_all = Post.objects.all()
+
+    return render(request, 'blog/index.html', context={'posts': posts_all})
+
+
+def post_detail(request, slug):
+    post_details = Post.objects.get(slug__iexact=slug)
+
+    return render(request, 'blog/post_detail.html', context={'post': post_details})

6055d9a2051db8baecf43c90857ebd08ac0764f8
Igor Kolodiazhnyi <ikolodiazhnyi@gmail.com>
2020-07-24 17:11:46 +0300

    Add urls. Add views
    index.html -- use 'include' built-in template tag
    urls.py -- added 'tags/' and 'tag/<str:slug>/' urls
    views.py -- added 'tags_list' to return all tags and 'tag_detail' to return tag with certain slug

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 
-from .models import Post
+from .models import Post, Tag
 
 
 def posts_list(request):
     posts_all = Post.objects.all()
-
     return render(request, 'blog/index.html', context={'posts': posts_all})
 
 
 def post_detail(request, slug):
-    post_details = Post.objects.get(slug__iexact=slug)
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
 
-    return render(request, 'blog/post_detail.html', context={'post': post_details})
+
+def tags_list(request):
+    tags_all = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags_all})
+
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'blog/tag_detail.html', context={'tag': tag})

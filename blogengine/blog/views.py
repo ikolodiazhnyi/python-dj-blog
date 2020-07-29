33c0976854037ef2242e3fa09935ee1f28c590f1
Igor Kolodiazhnyi <itsfunnytoseeyouhere@gmail.com>
2020-07-29 18:15:47 +0300

    Change views.py and urls.py
    - use get_object_or_404() to get 404 error in case of specific post or tag isn't available
    - use views classes PostDetail and TagDetail instead of views functions post_detail() and tag_detail() to follow the DRY principle of software development
    - use views classes PostDetail and TagDetail instead of views functions to solve the problem with put, post, delete requests which can not be implemented in views functions

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
 
 from .models import Post, Tag
 
@@ -8,16 +9,18 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts_all})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
-
-
 def tags_list(request):
     tags_all = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags_all})
 
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class PostDetail(View):
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug__iexact=slug)
+        return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+class TagDetail(View):
+    def get(self, request, slug):
+        tag = get_object_or_404(Tag, slug__iexact=slug)
+        return render(request, 'blog/tag_detail.html', context={'tag': tag})

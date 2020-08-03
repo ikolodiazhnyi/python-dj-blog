f48a3ff6ba7dc7a1955df54f967461b9cb0b7d4d
Igor Kolodiazhnyi <itsfunnytoseeyouhere@gmail.com>
2020-08-03 18:44:18 +0300

    Create mixin
    Useing class mixin instead of two CBV functions to reduce amount of code.

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import View
+from .utils import ObjectDetailMixin
 
 from .models import Post, Tag
 
@@ -14,13 +15,11 @@ def tags_list(request):
     return render(request, 'blog/tags_list.html', context={'tags': tags_all})
 
 
-class PostDetail(View):
-    def get(self, request, slug):
-        post = get_object_or_404(Post, slug__iexact=slug)
-        return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
 
 
-class TagDetail(View):
-    def get(self, request, slug):
-        tag = get_object_or_404(Tag, slug__iexact=slug)
-        return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'

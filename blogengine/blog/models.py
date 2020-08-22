cc9b3e40c19a57dc829714e0714e17fedbd38ff1
Igor Kolodiazhnyi <itsfunnytoseeyouhere@gmail.com>
2020-08-22 14:55:15 +0300

    Create slug generation function
    Use slugify method to create slug out of string then concatinate time to it.

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.utils.text import slugify
+from time import time
+
+
+def gen_slug(s):
+    new_slug = slugify(s, allow_unicode=True)
+    return new_slug + str(int(time()))
 
 
 # Create your models here.
